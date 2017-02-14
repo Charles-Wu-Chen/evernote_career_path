fdbd995158bd36a0898a2f88f22f5cdd17511143
CharlesWuChen <charles.wu.2000@gmail.com>
2017-02-14 11:44:19 +1100

    switch to tomcat container

--- evernote-eureka-server/src/main/java/io/pivotal/microservices/services/registration/RegistrationServer.java
+++ evernote-eureka-server/src/main/java/io/pivotal/microservices/services/registration/RegistrationServer.java
@@ -2,16 +2,25 @@ package io.pivotal.microservices.services.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 
 @SpringBootApplication
 @EnableEurekaServer
-public class RegistrationServer {
-	public static void main (String[] args ){
-	    // Tell Boot to look for registration-server.yml
-	    System.setProperty("spring.config.name", "registration-server");
-	    SpringApplication.run(RegistrationServer.class, args);
+public class RegistrationServer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RegistrationServer.class).properties("spring.config.name:registration-server");
+	}
+
+	public static void main(String[] args) {
+		// Tell Boot to look for registration-server.yml
+		System.setProperty("spring.config.name", "registration-server");
+		//SpringApplication.run(RegistrationServer.class, args);
+		
+		 new SpringApplicationBuilder(RegistrationServer.class)
+         .properties("spring.config.name:registration-server").build().run(args);
 	}
 }
-
