6e4729985a5106292a9281544967d82606ba03bb
CharlesWuChen <charles.wu.2000@gmail.com>
2017-02-17 12:02:22 +1100

    put both eureka server and client to docker

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # evernote_career_path
 project to practise according to requirement in evernote NOTE:"career path"
+
+
+start eureka
+docker run -d --name=eureka -p 8888:8080 evernote/eureka
+
+start account server
+docker run -d --name=account-service --env-file=./environment  -p 2222:2222 evernote/account-service
--- evernote-account-service/src/main/java/io/microservices/services/AccountServiceServer.java
+++ evernote-account-service/src/main/java/io/microservices/services/AccountServiceServer.java
@@ -3,16 +3,19 @@ package io.microservices.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 import io.microservices.services.accounts.AccountRepository;
 import io.microservices.services.accounts.AccountsConfiguration;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableDiscoveryClient
 @Import(AccountsConfiguration.class)
-public class AccountServiceServer {
+public class AccountServiceServer extends SpringBootServletInitializer{
 
 	
 	@Autowired
@@ -20,6 +23,13 @@ public class AccountServiceServer {
 	
 	public static void main (String [] args){
 		System.setProperty("spring.config.name", "account-service");
-		SpringApplication.run(AccountServiceServer.class, args);
+		new SpringApplicationBuilder(AccountServiceServer.class)
+        .properties("spring.config.name:account-service").build().run(args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AccountServiceServer.class).properties("spring.config.name:account-service");
+	}
+
 }
